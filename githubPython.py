39c41c2c463be38653d2eb96221d38d265e544e4
YuriZhdanov <87841698+YuriZhdanov@users.noreply.github.com>
2021-10-21 23:24:03 +0900

    aaa

--- githubPython.py
+++ githubPython.py
@@ -1,15 +1,10 @@
-import git
-import datetime
-import os
-from time import *
-from os import path
-from git import Repo
-
-
-curr_dir = os.path.dirname(os.path.realpath(__file__))
-repo = Repo(curr_dir)
-
-origin = repo.remote()
-print(origin)
-repo.create_head('my_new_branch')
-# origin.push('my_new_branch')
+import requests
+headers = {'Authorization': "Token " + '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'}
+url = "https://api.github.com/repos/YuriZhdanov/gitTest/git/refs/heads"
+branches = requests.get(url, headers=headers).json()
+branch, sha = branches[-1]['ref'], branches[-1]['object']['sha']
+res = requests.post('https://api.github.com/repos/YuriZhdanov/gitTest/git/refs', json={
+    "ref": "refs/heads/newbranch",
+    "sha": sha
+}, headers=headers)
+print(res.content)
